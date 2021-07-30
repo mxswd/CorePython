8f3c0a3cd843ae3d3718d07c2f650c2223e2d078
buildroot <buildroot@maxmini.local>
2021-07-30 13:31:35 +1200

    init

---
+++ CorePythonMacOS/CorePythonMacOS.h
@@ -0,0 +1,18 @@
+//
+//  CorePythonMacOS.h
+//  CorePythonMacOS
+//
+//  Created by buildroot on 22/06/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CorePythonMacOS.
+FOUNDATION_EXPORT double CorePythonMacOSVersionNumber;
+
+//! Project version string for CorePythonMacOS.
+FOUNDATION_EXPORT const unsigned char CorePythonMacOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CorePythonMacOS/PublicHeader.h>
+
+
