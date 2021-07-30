b698d29b32675c77aac5d786e21a8329545abd49
Maxwell <136101+mxswd@users.noreply.github.com>
2021-07-30 18:40:01 +1200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         .binaryTarget(
             name: "CorePython",
             url: "https://github.com/mxswd/CorePython/releases/download/3.9-m2/CorePython.xcframework.zip",
-            checksum: "c53ce032d43217a123ecbcffbb0f923d2b038bdfe9a496886eacc0012203709b"
+            checksum: "51830aa3e6297b32a5c72227d1c192bb512ce81d8bcb9bf618f938072c5c9d79"
         )
     ]
 )
